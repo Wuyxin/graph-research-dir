54ceb9e993bd0ebe19027f9942d2ce9401f35786
Shirley Wu <shirwu@hyperturing2.stanford.edu>
2023-03-16 21:58:02 -0700

    update

--- generate_wordcloud.py
+++ generate_wordcloud.py
@@ -7,35 +7,73 @@ Generating a square wordcloud from the US constitution using default arguments.
 
 import os
 from os import path
+import matplotlib
 import matplotlib.pyplot as plt
 
 from wordcloud import WordCloud
+import nltk
 
-year = 2023
+from nltk.tokenize import word_tokenize
+from nltk.probability import FreqDist
+from nltk.tokenize import word_tokenize
+nltk.download('punkt')
 
-# get data directory (using getcwd() is needed to support running example in generated IPython notebook)
-d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
+fdists = {}
+interest_list = ['molecule', 'protein', 'transformer', 'causal', 'KG', 'sample', 'generalization',\
+                 'contrastive', 'dynamic', 'spectral', 'distribution', 'domain', 'pretraining', 'architecture',\
+                 'scale', 'attention', 'unsupervised', 'semisupervised', 'pretraining', 'robustness',\
+                 'bias', 'explanation', 'generative', 'knowledgegraph']
 
-# Read the whole text.
-text = open(path.join(d, f'abstracts/{year}.txt')).read()
+font = {
+        'weight' : 'bold',
+        'size'   : 14,
+        }
 
-# Generate a word cloud image
-# wordcloud = WordCloud().generate(text)
+matplotlib.rc('font', **font)
+for year in [2021, 2022, 2023]:
 
-# Display the generated image:
-# the matplotlib way:
-# plt.imshow(wordcloud, interpolation='bilinear')
-# plt.axis("off")
+    # get data directory (using getcwd() is needed to support running example in generated IPython notebook)
+    d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
 
-# lower max_font_size
-wordcloud = WordCloud(max_font_size=60, width=1200, height=700).generate(text)
-plt.figure(figsize=(12,7))
-plt.imshow(wordcloud, interpolation="bilinear")
-plt.axis("off")
-plt.tight_layout()
-plt.savefig(f'figures/{year}.png', dpi=1000)
-plt.show()
+    # Read the whole text.
+    all_text = open(path.join(d, f'abstracts/{year}.txt')).read().replace('-', '')
+    all_text = all_text.replace('knowledge graph', 'knowledgegraph')
+    lines = open(path.join(d, f'abstracts/{year}.txt')).readlines()
+    text_list = [list(set(word_tokenize(line.lower().replace('-', '').replace('knowledge graph', 'knowledgegraph')))) for line in lines]
+    single_list = []
+    for _list in text_list:
+        single_list.extend(_list)
+    text = ' '.join([' '.join(_list) for _list in text_list])
+    
+    # lower max_font_size
+    if False:
+        wordcloud = WordCloud(max_font_size=60, width=1200, height=700).generate(all_text)
+        plt.figure(figsize=(12,7))
+        plt.imshow(wordcloud, interpolation="bilinear")
+        plt.axis("off")
+        plt.tight_layout()
+        plt.savefig(f'figures/{year}.png', dpi=500)
+        plt.cla()
+    
+    text_list = []
+    for w in single_list:
+        if w == 'sampling' or w == 'samples' or w == 'sample':
+            text_list.append('sample')
+            continue
+        for i_w in interest_list:
+            if i_w in w:
+                if i_w == 'knowledgegraph':
+                    text_list.append('knowledge graph')
+                else:
+                    text_list.append(i_w)
+                break
+    fdist = FreqDist(text_list)
+    plt.figure(figsize=(10,6))
+    
+    fdist.plot(30, title=f'ICLR {year} - Frequency words in GNN research')
+    plt.tight_layout()
+    plt.xticks(rotation=90)
+    plt.savefig(f'figures/{year}-dist.png', dpi=300)
+    plt.cla()
+    fdists[year] = fdist
 
-# The pil way (if you don't have matplotlib)
-# image = wordcloud.to_image()
-# image.show()
--- main.py
+++ main.py
@@ -4,61 +4,61 @@ import pandas as pd
 from tqdm import tqdm
 
 client = openreview.Client(baseurl='https://api.openreview.net')
-year = 2021
+for year in [2021, 2022, 2023]:
 
-submissions = client.get_all_notes(invitation=f"ICLR.cc/{year}/Conference/-/Blind_Submission", details='directReplies')
+    submissions = client.get_all_notes(invitation=f"ICLR.cc/{year}/Conference/-/Blind_Submission", details='directReplies')
 
-reviews = []
-for submission in submissions:
-    reviews = reviews + [reply for reply in submission.details["directReplies"] if reply["invitation"].endswith("Official_Review")]
+    reviews = []
+    for submission in submissions:
+        reviews = reviews + [reply for reply in submission.details["directReplies"] if reply["invitation"].endswith("Official_Review")]
 
-scores_dict = {}
-for r in reviews:
-    if r['forum'] not in scores_dict:
-        scores_dict[r['forum']] = []
-    try:
-        scores_dict[r['forum']].append(int(r['content']['recommendation'].split(':')[0]))
-    except:
-        scores_dict[r['forum']].append(int(r['content']['rating'].split(':')[0]))
+    scores_dict = {}
+    for r in reviews:
+        if r['forum'] not in scores_dict:
+            scores_dict[r['forum']] = []
+        try:
+            scores_dict[r['forum']].append(int(r['content']['recommendation'].split(':')[0]))
+        except:
+            scores_dict[r['forum']].append(int(r['content']['rating'].split(':')[0]))
 
-statistics = []
-all_data, abstracts = [], []
-for s in tqdm(submissions):
-    abstract = s.content['abstract']
-    keywords = s.content['keywords']
-    if s.forum not in scores_dict:
-        continue
-    title = s.content['title']
-    if 'Please_choose_the_closest_area_that_your_submission_falls_into' in s.content:
-        area = s.content['Please_choose_the_closest_area_that_your_submission_falls_into']
-    else:
-        area = ''
-    scores = scores_dict[s.forum]
-    avg_score = np.mean(scores)
-    std_score = np.std(scores)
-    _title = title.lower()
-    _keywords = [k.lower() for k in keywords]
-    _abstract = abstract.lower()
-    if (
-        (('graph ' in _keywords) or ('graph ' in _abstract) or ('graph ' in _title) or \
-        (' graph' in _keywords) or (' graph' in _abstract) or (' graph' in _title)) and \
-        (not 'graphics' in _keywords) and (not 'graphics' in _abstract) and (not 'graphics' in _title)
-    ) or \
-    ('gnn' in _keywords) or ('gnn' in _abstract) or ('gnn' in _title) or \
-    ('gcn' in _keywords) or ('gcn' in _abstract) or ('gcn' in _title):
-            
-        all_data.append([ title, str(avg_score), str(std_score), ';'.join([str(i) for i in scores]), area, keywords, abstract])
-        abstracts.append(abstract)
+    statistics = []
+    all_data, abstracts = [], []
+    for s in tqdm(submissions):
+        abstract = s.content['abstract']
+        keywords = s.content['keywords']
+        if s.forum not in scores_dict:
+            continue
+        title = s.content['title']
+        if 'Please_choose_the_closest_area_that_your_submission_falls_into' in s.content:
+            area = s.content['Please_choose_the_closest_area_that_your_submission_falls_into']
+        else:
+            area = ''
+        scores = scores_dict[s.forum]
+        avg_score = np.mean(scores)
+        std_score = np.std(scores)
+        _title = title.lower()
+        _keywords = [k.lower() for k in keywords]
+        _abstract = abstract.lower()
+        if (
+            (('graph ' in _keywords) or ('graph ' in _abstract) or ('graph ' in _title) or \
+            (' graph' in _keywords) or (' graph' in _abstract) or (' graph' in _title)) and \
+            (not 'graphics' in _keywords) and (not 'graphics' in _abstract) and (not 'graphics' in _title)
+        ) or \
+        ('gnn' in _keywords) or ('gnn' in _abstract) or ('gnn' in _title) or \
+        ('gcn' in _keywords) or ('gcn' in _abstract) or ('gcn' in _title):
+                
+            all_data.append([ title, str(avg_score), str(std_score), ';'.join([str(i) for i in scores]), area, keywords, abstract])
+            abstracts.append(abstract)
 
 
-df = pd.DataFrame(all_data, columns=['Title', 'Average Score', 'Standard Deviation', 'Individual Scores', 'Author-defined Area', 'Keywords', 'Abstract'])
-df = df.sort_values(by=['Average Score'], ascending=False, ignore_index=True)
-df.index = np.arange(1, len(df)+1)
-df.to_csv('output.csv', index='True')
- 
-# writing to file
-txt = ''
-for abstract in abstracts:
-    txt += abstract + '\n'
-with open(f'abstracts/{year}.txt', 'w') as f:
-    f.write(txt)
+    df = pd.DataFrame(all_data, columns=['Title', 'Average Score', 'Standard Deviation', 'Individual Scores', 'Author-defined Area', 'Keywords', 'Abstract'])
+    df = df.sort_values(by=['Average Score'], ascending=False, ignore_index=True)
+    df.index = np.arange(1, len(df)+1)
+    df.to_csv('output.csv', index='True')
+    
+    # writing to file
+    txt = ''
+    for abstract in abstracts:
+        txt += abstract + '\n'
+    with open(f'abstracts/{year}.txt', 'w') as f:
+        f.write(txt)
